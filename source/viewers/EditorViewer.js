16290ab986e824616e4aef31f581db71a6e3b0e1
Heng <31088159+shiukaheng@users.noreply.github.com>
2020-11-18 22:45:10 +0800

    Revert "Revert "Started work on EditorViewer""
    This reverts commit 5b44bbb04a8c670bdd1e1612b892dd5a7d9871bb.

--- source/viewers/EditorViewer.js
+++ source/viewers/EditorViewer.js
@@ -1,27 +1,21 @@
 var Viewer = require("./Viewer")
 var THREE = require("three")
 class EditorViewer extends Viewer {
-    constructor(...args) {
-        super(...args)
+    constructor(containerElement) {
+        super(containerElement, false)
         this.floorMat = new THREE.MeshBasicMaterial({wireframe:true, color:"grey"})
         this.floorGeom = new THREE.PlaneGeometry(200,200,100,100)
         this.floor = new THREE.Mesh(this.floorGeom, this.floorMat)
         this.floor.rotation.x = Math.PI/2
         this.scene.add(this.floor)
+        this.UIInitialized = false
     }
-    update(dt, playerOnly=false){
-        this._args.objects.forEach(object => {
-            if (object.assetsLoaded) {
-                if (playerOnly===true) {
-                    if (object.isPlayerObject) {
-                        object.update(dt)
-                    }
-                } else {
-                    object.update(dt)
-                }
-                
-            }
-        })
+    initUI() {
+        if (this.UIInitialized) {
+            return
+        }
+    }
+    discardUI() {
     }
 }
 module.exports = EditorViewer
