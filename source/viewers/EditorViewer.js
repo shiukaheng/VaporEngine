2298e59fd890da7efdafa857a348201c32910498
Heng <31088159+shiukaheng@users.noreply.github.com>
2020-11-20 12:14:53 +0800

    Started working on creation wizard

--- source/viewers/EditorViewer.js
+++ source/viewers/EditorViewer.js
@@ -17,20 +17,19 @@ class EditorViewer extends Viewer {
         this.UIContainer = document.createElement("div")
         this.UIContainer.classList.add("vapor-editor-overlay")
         this.containerElement.appendChild(this.UIContainer)
-        var ar = new ButtonRow([new Button(()=>{ar.parent.add(br); ar.disabled=true},"Add"), new Button(undefined,"Select"), new Button(undefined,"Select from list"), new Button(undefined,"Settings")])
+        var ar = new ButtonRow([new Button(()=>{ar.parent.add(br)},"Add"), new Button(undefined,"Select"), new Button(undefined,"Select from list"), new Button(undefined,"Settings")])
         var scm = Serializable.getSerializableClassesManager()
         var buttonList = []
         Object.keys(scm.classList).forEach(className=>{
             if (scm.classList[className].prototype instanceof scm.classList["BaseObject"]) {
                 buttonList.push(new Button(()=>{
-                    ar.disabled = false
                     br.parent.remove(br)
                 }, className))
             }
         })
         var br = new ButtonRow(buttonList)
         // var br = new ButtonRow([new Button(()=>{ar.disabled = false; br.parent.remove(br)}, "Option")])
-        var ab = new ButtonRowStack([ar])
+        var ab = new RowStack([ar], true)
         this.UIContainer.appendChild(ab.domElement)
     }
 }
@@ -107,13 +106,15 @@ class ButtonRow {
     }
 }
 
-class ButtonRowStack {
-    constructor(listOfButtonRows=[]) {
+class RowStack {
+    constructor(listOfRows=[], onlyEnableLastRow=false) {
         this._disabled = false
+        this._onlyEnableLastRow = onlyEnableLastRow
+        this._rowList = []
         this.domElement = document.createElement("div")
         this.domElement.classList.add("vapor-editor-button-row-stack")
-        listOfButtonRows.forEach(buttonRows=>{
-            this.add(buttonRows)
+        listOfRows.forEach(rows=>{
+            this.add(rows)
         })
     }
     get disabled() {
@@ -128,23 +129,121 @@ class ButtonRowStack {
             this._disabled = false
         }
     }
-    add(buttonRow) {
-        this.domElement.appendChild(buttonRow.domElement)
-        var ogHeight = buttonRow.domElement.clientHeight // Todo: try method that doesnt use clientHeight, instead, transition using em sizes.
+    add(row) {
+        this.domElement.appendChild(row.domElement)
+        this._rowList.push(row)
+        if (this._onlyEnableLastRow) {
+            this._rowList.slice(0,-1).forEach(row=>{
+                row.disabled = true
+            })
+        }
+        var ogHeight = row.domElement.clientHeight // Todo: try method that doesnt use clientHeight, instead, transition using em sizes.
         if (ogHeight!==0) {
-            buttonRow.domElement.style.maxHeight = "0px"
-            buttonRow.domElement.style.overflowY = "hidden" //
-            gsap.TweenLite.fromTo(buttonRow.domElement, {maxHeight: "0px"}, {maxHeight: ogHeight.toString()+"px", duration:0.5}) // Todo: Cancel animation if new animation happens
+            row.domElement.style.maxHeight = "0px"
+            row.domElement.style.overflowY = "hidden" //
+            gsap.TweenLite.fromTo(row.domElement, {maxHeight: "0px"}, {maxHeight: ogHeight.toString()+"px", duration:0.1}) // Todo: Cancel animation if new animation happens
         }
         
-        buttonRow.parent = this
+        row.parent = this
     }
-    remove(buttonRow) {
-        if (buttonRow.parent !== this) {
+    remove(row) {
+        if (row.parent !== this) {
             throw new Error("attempt to remove child from non-parent")
         }
-        this.domElement.removeChild(buttonRow.domElement)
-        buttonRow.parent = null
+        this.domElement.removeChild(row.domElement)
+        this._rowList.pop()
+        if (this._onlyEnableLastRow) {
+            this._rowList.slice(-1).forEach(row=>{
+                row.disabled = false
+            })
+        }
+        row.parent = null
+    }
+}
+
+function isFloat(val) {
+    var floatRegex = /^-?\d+(?:[.,]\d*?)?$/;
+    if (!floatRegex.test(val))
+        return false;
+
+    val = parseFloat(val);
+    if (isNaN(val))
+        return false;
+    return true;
+}
+
+function isInt(val) {
+    var intRegex = /^-?\d+$/;
+    if (!intRegex.test(val))
+        return false;
+
+    var intVal = parseInt(val, 10);
+    return parseFloat(val) == intVal && !isNaN(intVal);
+}
+
+class InputCell {
+    constructor(interpType="string", defaultStr, valCheck=()=>{return true}) {
+        if (!(interpType in this.supportedInterpTypes)) {
+            throw new Error("invalid interpType")
+        }
+        this.interpType = interpType
+        this._valCheck = valCheck
+        this._inputValue = null
+        this._defaultStr = this.castStrToDestType(defaultStr)
+        this._valid = true
+    }
+    get supportedInterpTypes() {
+        return ["string", "float", "int"]
+    }
+    castStrToDestType(str) {
+        var error = new Error("failed to cast to type")
+        switch(this.interpType) {
+            case "string":
+                return str
+            case "float":
+                if (!(isFloat(str))) {
+                    throw error
+                }
+                return parseFloat(str)
+            case "int":
+                if (!(isInt(str))) {
+                    throw error
+                }
+                return parseFloat(str)
+        }
+    }
+    valueCheck(val) {
+        return this._valCheck(val)
+    }
+    inputString(str) {
+        var val
+        try {
+            val = castStrToDestType(str)
+        } catch(err) {
+
+        }
+            
+    }
+    get value() {
+        // if invalid, return undefined
+    }
+    set value(val) {
+        // check if value matches type
+        // check if value matches valid check
+    }
+    get valid() {
+
+    }
+    onContentChanged() {
+        var content = "get content here"
+        // Interpret to destination type, if failed set valid = false
+        // Value check, if failed set valid = false
+    }
+}
+
+class ObjectCreationWizard {
+    constructor(className, onDone=()=>{return {"arg":"..."}}) {
+        this.className = className
     }
 }
 
